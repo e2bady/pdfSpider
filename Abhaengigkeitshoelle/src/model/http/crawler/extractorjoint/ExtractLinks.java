c34aa373f4ab7e957b0308fc05a110e6af0852c0
Sebastian van Wickern <e2.bady@gmail.com>
2013-07-22 11:07:01 +0200

    refactor. no dependency to http from extractlinks.

--- Abhaengigkeitshoelle/src/model/http/crawler/extractorjoint/ExtractLinks.java
+++ Abhaengigkeitshoelle/src/model/http/crawler/extractorjoint/ExtractLinks.java
@@ -20,19 +20,15 @@ public class ExtractLinks extends Lazy implements Extractor<URL> {
 	private final Document document;
 	private Set<URL> links = null;
 	
-	public ExtractLinks(Document httpCon) {
-		this.document = httpCon;
+	public ExtractLinks(Document doc) {
+		this.document = doc;
 	}
-	/* (non-Javadoc)
-	 * @see model.http.crawler.Extractor#getLinks()
-	 */
 	public Set<URL> get() {
 		super.lazyLoad();
 		return Collections.unmodifiableSet(links);
 	}
 	@Override
 	protected boolean load() {
-		if(links != null) return true;
 		links = new HashSet<URL>();
 		Elements select = this.document.select("body a");
 		for(Element e : select) {
@@ -42,7 +38,6 @@ public class ExtractLinks extends Lazy implements Extractor<URL> {
 				links.add(new URL(absUrl));
 			} catch (MalformedURLException e1) {
 				log.error("url was not valid: " + absUrl);
-				e1.printStackTrace();
 			}
 		}
 		return true;
--- Abhaengigkeitshoelle/src/unittests/ExtractLinksTest.java
+++ Abhaengigkeitshoelle/src/unittests/ExtractLinksTest.java
@@ -4,62 +4,70 @@ import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.Set;
 
 import model.http.crawler.extractorjoint.ExtractLinks;
 import model.http.crawler.extractorjoint.Extractor;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+
 public class ExtractLinksTest {
+	private static final String http = "<html><head></head><body>" +
+			"<a href=\"http://www.google.com\">google</a>" +
+			"<a href=\"/internallink.htm\">internal</a>" +
+			"<a href=\"./relative.htm\">internalrelativ</a>" +
+			"</body></html>";
+	private static final String http2 = "<html><head></head><body>" +
+			"<a href=\"http://www.google.com\">google</a>" +
+			"<a href=\"/internallink.htm\">internal</a>" +
+			"<a href=\"./relative.htm\">internalrelativ</a>" +
+			"<a href=\"relative.htm\">internalrelativ</a>" +
+			"</body></html>";
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 	@Test
+	public final void notnull() {
+		Document parse = Jsoup.parse("");
+		parse.setBaseUri("http://www.tkse.de/foo/bar.htm");
+		Extractor<URL> links = new ExtractLinks(parse);
+		assertNotNull(links.get());
+	}
+	@Test
 	public final void testImmutability() throws MalformedURLException {
-		Extractor<URL> links = setUpExtractLinks();
+		Document parse = Jsoup.parse(http);
+		parse.setBaseUri("http://www.tkse.de/foo/bar.htm");
+		Extractor<URL> links = new ExtractLinks(parse);
 		Set<URL> refs = links.get();
 		exception.expect(UnsupportedOperationException.class);
 		refs.add(new URL("http://www.google.com"));
 	}
 	@Test
-	public final void testExtract() throws MalformedURLException {
-		Extractor<URL> links = setUpExtractLinks();
-		exception.expect(MalformedURLException.class);
-		URL url = new URL("www.tks.de/page/index.html");
-		links = new ExtractLinks(new MockHttpGet().get(url));
-		links.get();
-	}
-	@Test
 	public final void testgetLinks() throws MalformedURLException {
-		Extractor<URL> links = setUpExtractLinks();
+		Document parse = Jsoup.parse(http);
+		parse.setBaseUri("http://www.tkse.de/foo/bar.htm");
+		Extractor<URL> links = new ExtractLinks(parse);
 		if(links.get().size() != 3) fail("Links weren't parsed correctly.");
 	}
-	private Extractor<URL> setUpExtractLinks() throws MalformedURLException {
-		MockHttpGet mockHttpGet = new MockHttpGet();
-		URL url = new URL("http://www.tks.de/page/index.html");
-		Extractor<URL> links = new ExtractLinks(mockHttpGet.get(url));
-		links.get();
-		return links;
-	}
-	@Test
-	public final void testReanalysation() throws MalformedURLException {
-		MockHttpGet mockHttpGet = new MockHttpGet();
-		URL url = new URL("http://www.tks.de/page/index.html");
-		Extractor<URL> links = new ExtractLinks(mockHttpGet.get(url));
-		Set<URL> refs = links.get();
-		mockHttpGet.setMode(1);
-		Set<URL> refsReanalyse = links.get();
-		for(URL link : refsReanalyse) {
-			if(!refs.contains(link)) fail("ExtractLinks reanalysed content!!");
-		}
-	}
-	
 	@Test
 	public final void testBaseURI() throws MalformedURLException {
-		Extractor<URL> links = setUpExtractLinks();
+		@SuppressWarnings({ "serial" })
+		Set<URL> expected = new HashSet<URL>() {{
+			this.add(new URL("http://www.google.com"));
+			this.add(new URL("http://www.example.com/internallink.htm"));
+			this.add(new URL("http://www.example.com/bar/relative.htm"));
+			}};
+		Document parse = Jsoup.parse(http2);
+		parse.setBaseUri("http://www.example.com/bar/somepage.htm");
+		Extractor<URL> links = new ExtractLinks(Jsoup.parse(http2));
 		Set<URL> refs = links.get();
-		for(URL link : refs)
-			System.err.println(link);
+		for(URL link : refs) {
+			if(!expected.contains(link)) fail("link gotten was not found within expected.");
+		}
 	}
 }
