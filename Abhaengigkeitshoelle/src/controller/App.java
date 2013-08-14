11ee364e76fd5fa3298f6f83ed552783ba1043e0
Sebastian van Wickern <e2.bady@gmail.com>
2013-08-14 12:26:35 +0200

    reorganization

--- Abhaengigkeitshoelle/src/controller/App.java
+++ Abhaengigkeitshoelle/src/controller/App.java
@@ -17,16 +17,21 @@ public class App {
 	 * @throws  
 	 */
 	public static void main(String[] args) throws MalformedURLException {
+		String dataNamespace = "http://juris\\.bundesgerichtshof\\.de/cgi-bin/rechtsprechung/document\\.py?.*\\.pdf";
+		String startAt = "http://juris.bundesgerichtshof.de/cgi-bin/rechtsprechung/list.py?Gericht=bgh&Art=en&Datum=Aktuell&Sort=12288";
+		String titleRegex = "([\\d]+ )?[ARs|V ZR|StR|AnwZ (B)|I ZB|StR]+ [\\d\\/]+";
+		String dateRegex = "([\\d]{1,2}\\. [a-zA-Z]+ [\\d]{4}|[\\d]{1,2}\\. [a-zA-Z]+ [\\d]{2})";
+		String category = "Recht";
+		String type = "BGH";
+		
 		IProxySetter proxy = ProxyFactory.createProxy();
 		IDB db = DbFactory.createDB();
 		String crawlNamespace = "http://juris\\.bundesgerichtshof\\.de/cgi-bin/rechtsprechung/.*";
-		String dataNamespace = "http://juris\\.bundesgerichtshof\\.de/cgi-bin/rechtsprechung/document\\.py?.*\\.pdf";
-		String startAt = "http://juris.bundesgerichtshof.de/cgi-bin/rechtsprechung/list.py?Gericht=bgh&Art=en&Datum=Aktuell&Sort=12288";
 		
 		IDataRetriever retriever = ModelFactory
 				.createMySQLHttpRetriever(proxy, db, 
-						crawlNamespace, dataNamespace, 
-						startAt, new ResultFactoryImpl("Recht", "([\\d]+ )?[ARs|V ZR|StR|AnwZ (B)|I ZB|StR]+ [\\d\\/]+", "([\\d]{1,2}\\. [a-zA-Z]+ [\\d]{4}|[\\d]{1,2}\\. [a-zA-Z]+ [\\d]{2})"), "BGH");
+				crawlNamespace, dataNamespace, startAt, 
+				new ResultFactoryImpl(category, titleRegex, dateRegex), type);
 		try {
 			retriever.crawl(100);
 		} finally {
